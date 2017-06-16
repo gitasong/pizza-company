b541aea156f571a0ef54be345e5717b293e7dde2
Nicole Freed <nicole.freed@gmail.com>
2017-06-16 09:06:37 -0700

    create document.ready and submit functions with event.preventDefault()

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// business logic goes here
+
+
+
+
+// user-interface logic goes here
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    
+  });
+});
