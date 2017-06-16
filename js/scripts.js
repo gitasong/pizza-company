fe90be573bcb4a04a7d89f61c5ebce7fc9af6950
Nicole Freed <nicole.freed@gmail.com>
2017-06-16 09:24:06 -0700

    implement value capture from dropdown; fix bugs preventing form submission and cost display

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,14 @@
 
 // user-interface logic goes here
 $(document).ready(function() {
+  // console.log("hello");
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
-    
+      // console.log("hello");
+
+    var inputtedPizzaSize = $("#pizza-size").val();
+      // console.log(inputtedPizzaSize);
+
+    $(".display-cost").show();
   });
 });
