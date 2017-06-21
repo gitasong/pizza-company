ebbc9a5d666b73b7ece2bf6816502050c4ff072d
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-20 18:39:45 -0700

    cleanup and commenting

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 // business logic goes here
+// Pizza object prototype
 function Pizza(pizzaSize, toppings) {
   this.pizzaSize = pizzaSize;
   this.toppings = toppings;
 };
 
+// prototype method to get base pizza price from pizza size
 Pizza.prototype.getPizzaPrice = function() {
-  console.log("this.pizzaSize = " + this.pizzaSize);
   return this.pizzaSize += 2;
 };
 
+// prototype method to get combined price of toppings
 Pizza.prototype.getToppingsPrice = function() {
-  console.log("this.toppings = " + this.toppings);
   var toppingsArr = this.toppings;
   var price = 0;
   var runningTotal = 0;
-  console.log("this.toppingsArr = " + toppingsArr);  // WORKING
   this.toppings.forEach(function(topping) {  // WORKING??
     if (topping === "pepperoni" || topping === "sausage") {
       price = 1.79;
@@ -25,19 +25,16 @@ Pizza.prototype.getToppingsPrice = function() {
     };
     runningTotal += price;
   });
-  // console.log("runningTotal = " + runningTotal);
   return runningTotal;
 };
 
-
+// prototype method to get total cost of pizza from base pizza price + toppings
 Pizza.prototype.getCost = function() {
-  // console.log("this.getPizzaPrice = " + this.getPizzaPrice());
-  // console.log("this.getToppingsPrice = " + this.getToppingsPrice());
-  return this.getPizzaPrice() + this.getToppingsPrice();  // * undefined; I have no idea why
+  return this.getPizzaPrice() + this.getToppingsPrice();
 };
 
+// resets form fields with empty string to clear them
 function resetFields() {
-  // resets form fields with empty string to clear them
   $("#pizza-size").val("");
   $("input:checkbox[name=pizza-toppings]").prop("checked", false);
 };
@@ -49,18 +46,16 @@ $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
 
-    var inputtedPizzaSize = parseInt($("#pizza-size").val());  // gets pizza-size value (working)
-      console.log(inputtedPizzaSize);  // WORKING
+    var inputtedPizzaSize = parseInt($("#pizza-size").val());  // gets pizza-size value
 
+    // gets pizza toppings; pushes to toppingsArr
     var toppingsArr = [];
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
       var topping = $(this).val();
       toppingsArr.push(topping);
-    });  // gets pizza toppings; pushes to toppingsArr (working)
-      console.log(toppingsArr);  // WORKING
+    });
 
-    var newPizza = new Pizza(inputtedPizzaSize, toppingsArr);  // * this - creates new Pizza from inputted data (inputtedPizzaSize and toppingsArr working)
-      console.log(newPizza);  // WORKING
+    var newPizza = new Pizza(inputtedPizzaSize, toppingsArr);  // creates new Pizza from inputted data
 
     resetFields();
 
@@ -68,7 +63,6 @@ $(document).ready(function() {
     $(".display-cost").show();
 
     $("span#order-details").text(inputtedPizzaSize + "-inch with " + toppingsArr);
-    // console.log("Total cost: " + newPizza.getCost());
     $("span#pizza-cost").text(newPizza.getCost());
   });
 });
